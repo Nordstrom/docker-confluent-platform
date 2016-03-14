96cfcd6d8b59b1752811097d76c8235ebde1308b
Emmanuel Gomez <emmanuel@gomez.io>
2016-03-13 17:17:04 -0700

    Switch to the official name: `LOG_DIR` (not `KAFKA_LOGS_DIR`).

--- kafka/kafka-docker.sh
+++ kafka/kafka-docker.sh
@@ -57,11 +57,11 @@ for var in $(env | grep '^KAFKA_' | grep -v '^KAFKA_LOGS' | grep -v '^KAFKA_LOG4
   echo "${key}=${value}" >> ${kafka_cfg_file}
 done
 
-mkdir -p "${KAFKA_LOG_DIRS}" "${KAFKA_LOGS_DIR}"
+mkdir -p "${KAFKA_LOG_DIRS}" "${LOG_DIR}"
 chown -R "${CONFLUENT_USER}:${CONFLUENT_GROUP}" \
   /etc/kafka/log4j.properties \
   "${KAFKA_LOG_DIRS}" \
-  "${KAFKA_LOGS_DIR}" \
+  "${LOG_DIR}" \
   "${kafka_cfg_file}"
 
 /bin/gosu ${CONFLUENT_USER}:${CONFLUENT_GROUP} /usr/bin/kafka-server-start ${kafka_cfg_file}
